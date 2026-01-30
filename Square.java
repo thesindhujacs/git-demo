4de86df457f66c648195ecbd588365351516ae2a
thesindhujacs <sindhujacs1504@gmail.com>
2026-01-30 07:45:37 +0530

    Add Square class to calculate area of a square

---
+++ Square.java
@@ -0,0 +1,8 @@
+public class Square{
+  public static void main(String[] args){
+    double side=5.0;
+    double area=side*side;
+    System.out.println("Side: "+side);
+    System.out.println("Area: "+area);
+  }
+}
